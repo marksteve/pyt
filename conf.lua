8f713101c20c6bf317c2c1b90548d8f6ed32946a
Mark Steve Samson <hello@marksteve.com>
2015-12-25 15:01:03 +0800

    Initial commit

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+  t.window.title = "Pull Yourself Together!"
+  t.window.width = 360
+  t.window.height = 567
+end
---
+++ main.lua
@@ -0,0 +1,3 @@
+function love.draw()
+  love.graphics.print('Hello World!', 360 / 2, 567 / 2)
+end
